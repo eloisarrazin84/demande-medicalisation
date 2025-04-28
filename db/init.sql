9250f86bd46f6c986c8ac408e9f226d625d87b8e
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2025-04-28 15:42:32 +0200

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -1,21 +1,21 @@
 CREATE DATABASE IF NOT EXISTS medicalisation;
-
 USE medicalisation;
 
 CREATE TABLE IF NOT EXISTS demandes (
   id INT AUTO_INCREMENT PRIMARY KEY,
   evenement VARCHAR(255),
   date_evenement DATE,
-  lieu VARCHAR(255),
-  type_evenement VARCHAR(255),
   heure_debut TIME,
   heure_fin TIME,
+  lieu VARCHAR(255),
+  type_evenement VARCHAR(255),
   participants INT,
   public_cible VARCHAR(255),
   organisateur VARCHAR(255),
   responsable VARCHAR(255),
   telephone VARCHAR(20),
   email VARCHAR(255),
+  adresse_facturation TEXT,
   dispositif VARCHAR(255),
   nombre_personnels INT,
   materiel_specifique TEXT,
